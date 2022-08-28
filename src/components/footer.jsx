69567875a4e79329bd41ced891c46a9d2b8732b1
pablovr1000 <pablovr200@hotmail.com>
2022-08-27 21:04:04 -0300

    feat: Definindo pages

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h4>Desenvolvido por Pablo Vasconcelos Rios</h4>
+    </footer>
+  );
+}
---
+++ src/components/nav.bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h2 className="title">Sobre mim</h2>
+      <h2 className="title">Currículo</h2>
+      <h2 className="title">Projetos</h2>
+      <h2 className="title">Contato</h2>
+    </header>
+  );
+}
